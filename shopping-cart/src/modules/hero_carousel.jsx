ddff985e050e22e4f3717ce4228ec75b8b8946f3
Ioannis Boufogiannis <ioannisboufogiannis@gmail.com>
2024-05-07 19:13:45 +0300

    fixed carousel

--- shopping-cart/src/modules/hero_carousel.jsx
+++ shopping-cart/src/modules/hero_carousel.jsx
@@ -4,44 +4,48 @@ import Hero from "./hero";
 export default function HeroCarousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const heroes = [
-    <Hero
-      image={"/hero.jpg"}
-      subheader={"New Collection"}
-      header={"Summer 2024 FOS Collection"}
-      paragraph={`Get ready for the summer with our newest collection of cameras.
+    {
+      image: "/hero.jpg",
+      subheader: "New Collection",
+      header: "Summer 2024 FOS Collection",
+      paragraph: `Get ready for the summer with our newest collection of cameras.
           Lightweight, high-quality and perfect for capturing those special
           moments. Explore our new
           summer 2024 FOS collection
-          today.`}
-    />,
-    <Hero
-      subheader={"Landscape Collection"}
-      header={"Landscape Photography"}
-      image={"/hero_landscape.jpg"}
-      paragraph={`Enjoy the beauty of nature with our newest collection of cameras.
+          today.`,
+      color: "rgb(14 165 233)",
+    },
+    {
+      image: "/hero_landscape.jpg",
+      subheader: "Landscape Collection",
+      header: "Landscape Photography",
+      paragraph: `Enjoy the beauty of nature with our newest collection of cameras.
           Lightweight, high-quality and perfect for capturing the stunning
           landscapes and vistas. Capture the memories
           of your adventures with FOS
-          today.`}
-    />,
-    <Hero
-      subheader={"Portrait Collection"}
-      header={"Perfect for portraits"}
-      image={"/hero_portrait.jpg"}
-      paragraph={`Enjoy capturing those special moments with our portrait collection of cameras.
+          today.`,
+      color: "#616161",
+    },
+    {
+      image: "/hero_portrait.jpg",
+      subheader: "Portrait Collection",
+      header: "Perfect for portraits",
+      paragraph: `Enjoy capturing those special moments with our portrait collection of cameras.
           Ideal for capturing portraits and 
-          family photos. Capture every single smile and laugh with our cameras.`}
-    />,
-    <Hero
-      subheader={"Nature Collection"}
-      header={"Capture all the beauty of Nature"}
-      image={"/hero_nature.jpg"}
-      paragraph={`Capture the beauty of the world around you with our newest collection of cameras.
+          family photos. Capture every single smile and laugh with our cameras.`,
+      color: "#ffc24b",
+    },
+    {
+      image: "/hero_nature.jpg",
+      subheader: "Nature Collection",
+      header: "Capture all the beauty of Nature",
+      paragraph: `Capture the beauty of the world around you with our newest collection of cameras.
           Lightweight, high-quality and perfect for capturing the stunning
           landscapes, wildlife and vistas. Explore our new
           nature collection
-          today.`}
-    />,
+          today.`,
+      color: "#56bf0c",
+    },
   ];
 
   const goToPrev = () => {
@@ -69,6 +73,8 @@ export default function HeroCarousel() {
       clearInterval(interval);
     };
   }, [goToNext]);
+
+  const { subheader, header, paragraph } = heroes[currentIndex];
   return (
     <div className="relative flex items-center justify-center">
       {/* Previous Button */}
@@ -92,6 +98,41 @@ export default function HeroCarousel() {
         </svg>
       </button>
 
+      {/* Hero Carousel */}
+      <div
+        key={currentIndex}
+        className="flex flex-col justify-between items-center gap-2 px-20 py-8 h-[300px] w-[30vw] m-[] bg-[hsl(0,0%,96%,0.9)] rounded-md text-gray-800 fixed right-80 z-10"
+      >
+        <div className="flex flex-col gap-1 items-center">
+          <p className="text-gray-600 animate-fade-in">{subheader}</p>
+          <p className="font-bold text-3xl animate-fade-in">{header}</p>
+        </div>
+        <p className="text-base md:text-sm lg:text-base animate-fade-in">
+          {paragraph}
+        </p>
+        <button
+          style={{ backgroundColor: heroes[currentIndex].color }}
+          className="items-center transition-all ease-in duration-200 flex gap-2 hover:gap-4 hover:bg-sky-500 hover:transition-all hover:ease-out hover:duration-[500ms] px-5 py-3 text-white font-semibold rounded-md"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-arrow-right"
+          >
+            <line x1="5" y1="12" x2="19" y2="12"></line>
+            <polyline points="12 5 19 12 12 19"></polyline>
+          </svg>
+          Explore More
+        </button>
+      </div>
+
       {/* Carousel Slides */}
       <div className="overflow-hidden w-full">
         <div
@@ -100,7 +141,7 @@ export default function HeroCarousel() {
         >
           {heroes.map((hero, index) => (
             <div className="flex-shrink-0 w-full" key={index}>
-              {hero}
+              <Hero image={hero.image} />
             </div>
           ))}
         </div>
@@ -130,9 +171,8 @@ export default function HeroCarousel() {
       {/* Dots Navigation */}
       <div className="absolute bottom-6 w-24 h-2 rounded-lg bg-white opacity-70 shadow-sky-600">
         <div
-          className="h-full bg-sky-400  transition-all rounded-[3px] duration-500 ease-in-out"
+          className="h-full bg-sky-400 w-6  transition-all rounded-[3px] duration-500 ease-in-out"
           style={{
-            width: `(100 / ${heroes.length}) * (+${currentIndex + 1})` + "%",
             transform: `translateX(${currentIndex * (300 / (heroes.length - 1))}%)`,
             transition: "transform 0.5s ease-in-out", // Adjust the duration and timing function as needed
           }}
--- shopping-cart/tailwind.config.js
+++ shopping-cart/tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fade: {
+          "0%": {
+            opacity: 0,
+            filter: "blur(2px)",
+            transform: "translateX(-100px)",
+          },
+          "100%": {
+            opacity: 1,
+            filter: "blur(0px)",
+            transform: "translateX(0px)",
+          },
+        },
+      },
+      animation: {
+        "fade-in": "fade 0.3s ease-out",
+      },
+    },
   },
   plugins: [],
 };
